e0036ea5385cf06eab008281b51def15e4bd42e5
hw-ani <pcbmlh73@gmail.com>
2024-09-20 18:44:06 +0900

    서버 초기 설정

---
+++ server/src/main/java/knu/hackathon24/cat/thumbler/ThumblerApplication.java
@@ -0,0 +1,13 @@
+package knu.hackathon24.cat.thumbler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThumblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThumblerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/knu/hackathon24/cat/thumbler/ThumblerApplicationTests.java
@@ -0,0 +1,13 @@
+package knu.hackathon24.cat.thumbler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThumblerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
