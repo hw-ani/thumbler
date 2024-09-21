c31d28566347b4da4d7b29c36712eb95cb4cb365
hw-ani <pcbmlh73@gmail.com>
2024-09-21 18:40:19 +0900

    개발중...

--- server/src/main/java/knu/hackathon24/cat/thumbler/foods/FoodController.java
+++ server/src/main/java/knu/hackathon24/cat/thumbler/foods/FoodController.java
@@ -1,6 +1,7 @@
 package knu.hackathon24.cat.thumbler.foods;
 
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Comparator;
 import java.util.List;
 
@@ -9,12 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import knu.hackathon24.cat.thumbler.imageUpload.ImageUpload;
+import knu.hackathon24.cat.thumbler.session.Session;
+import knu.hackathon24.cat.thumbler.storeMember.StoreMember;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -24,6 +33,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequiredArgsConstructor
 public class FoodController {
   final private FoodRepository foodRepository;
+  final private ImageUpload imageUpload;
+  final private ObjectMapper objectMapper;
 
   private String[] areas = {"북구", "서구", "동구", "수성", "중구", "달서구", "남구", "달성군", "군위군"};
 
@@ -53,9 +64,9 @@ public class FoodController {
     return foodRepository.findById(Long.parseLong(foodId)).get();
   }
   
-  // 멀티파트
   @PostMapping()
-  public String postFood(@RequestParam MultipartFile image,
+  public String postFood(@CookieValue("sessionId") String sessionId,
+                         @RequestParam MultipartFile image,
                          @RequestParam String name,
                          @RequestParam Long originalPrice,
                          @RequestParam Long discountedPrice,
@@ -63,8 +74,20 @@ public class FoodController {
                          @RequestParam String description,
                          @RequestParam Long deadline) {
       // image upload 후 사진 url 가져오기
+      byte[] imageByte = {};
+      try {
+        imageByte = image.getBytes();
+      } catch(Exception e) {
+      }
+      String imageString = Base64.getEncoder().encodeToString(imageByte);
+      String imageUrl = imageUpload.uploadImage(imageString);
 
-      // 디비에 저장하기
+      // db에 저장하기
+      Session session = Session.getInstance();
+      StoreMember storeMember = session.getStoreMemberBySessionId(sessionId);
+      Food newFood = new Food(name, originalPrice, discountedPrice, count, deadline, imageUrl, storeMember.getStore());
+      foodRepository.save(newFood);
+
       return "success";
   }
 }
