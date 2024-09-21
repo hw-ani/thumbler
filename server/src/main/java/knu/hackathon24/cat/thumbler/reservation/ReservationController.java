3ad037c21786eecd4a8352eedd5427e325d89261
hw-ani <pcbmlh73@gmail.com>
2024-09-21 21:17:03 +0900

    마감 세일 예약 조회(사용자/가게) api

--- server/src/main/java/knu/hackathon24/cat/thumbler/reservation/ReservationController.java
+++ server/src/main/java/knu/hackathon24/cat/thumbler/reservation/ReservationController.java
@@ -1,5 +1,7 @@
 package knu.hackathon24.cat.thumbler.reservation;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import knu.hackathon24.cat.thumbler.foods.Food;
 import knu.hackathon24.cat.thumbler.foods.FoodRepository;
 import knu.hackathon24.cat.thumbler.session.Session;
+import knu.hackathon24.cat.thumbler.storeMember.StoreMember;
 import knu.hackathon24.cat.thumbler.userMember.UserMember;
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
@@ -44,19 +49,49 @@ public class ReservationController {
 
     return new ReserveResDTO(reservationCode);
   }
+
+  @GetMapping("/store")
+  public List<Reservation> getReservationListStore(@CookieValue("sessionId") String sessionId) {
+    List<Reservation> allReservations = reservationRepository.findAll();
+    StoreMember storeMember = session.getStoreMemberBySessionId(sessionId);
+    
+    List<Reservation> response = new ArrayList<Reservation>();
+    for (Reservation reservation : allReservations) {
+      if (reservation.getFood().getStore().getId() == storeMember.getStore().getId()) {
+        response.add(reservation);
+      }
+    }
+
+    return response;
+  }
+  
+  @GetMapping("/user")
+  public List<Reservation> getReservationListUser(@CookieValue("sessionId") String sessionId) {
+    List<Reservation> allReservations = reservationRepository.findAll();
+    UserMember userMember = session.getUserMemberBySessionId(sessionId);
+    
+    List<Reservation> response = new ArrayList<Reservation>();
+    for (Reservation reservation : allReservations) {
+      if (reservation.getUserMember().getId() == userMember.getId()) {
+        response.add(reservation);
+      }
+    }
+
+    return response;
+  }
   
   private String generateReservationCode() {
     Random random = new Random();
     StringBuilder sb = new StringBuilder();
     
     for (int i = 0; i < 2; i++) {
-        char randomChar = (char) (random.nextInt(26) + 'A');
-        sb.append(randomChar);
+      char randomChar = (char) (random.nextInt(26) + 'A');
+      sb.append(randomChar);
     }
     
     for (int i = 0; i < 8; i++) {
-        int randomDigit = random.nextInt(10);
-        sb.append(randomDigit);
+      int randomDigit = random.nextInt(10);
+      sb.append(randomDigit);
     }
     
     return sb.toString();
